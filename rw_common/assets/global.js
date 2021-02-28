ef9d2a4ef59b80015df854c4e0d6ed31fb665d43
Matias Agelvis <Magelvisdoz@gmail.com>
2021-02-27 20:32:29 -0400

    Improved navbar responsiveness

--- rw_common/assets/global.js
+++ rw_common/assets/global.js
@@ -1,3 +1,3 @@
 
-var headerHeight=$('#pageHeader').outerHeight();function scrollFunction(){if(document.body.scrollTop>headerHeight||document.documentElement.scrollTop>headerHeight){document.getElementById("navbar").style.top="0";}else{document.getElementById("navbar").style.top="-100px";}}
+var headerHeight=$('#pageHeader').outerHeight();function scrollFunction(){if(document.body.scrollTop>headerHeight||document.documentElement.scrollTop>headerHeight){document.getElementById("navbar").style.top="0";}else{document.getElementById("navbar").style.top="-"+$('#navbar').height()+"px";}}
 window.addEventListener("scroll",scrollFunction,{passive:true});
